ca556836653d1842b1749d866003e574bce434c3
NoamAdari <lisentgaming2006@gmail.com>
2022-11-06 19:25:41 +0200

    Yaniv

--- src/main/java/art/kadawi/stayalive/commands/ShowDeath.java
+++ src/main/java/art/kadawi/stayalive/commands/ShowDeath.java
@@ -1,10 +1,13 @@
 package art.kadawi.stayalive.commands;
 
+import art.kadawi.stayalive.StayAlive;
+import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
 public class ShowDeath implements CommandExecutor {
@@ -12,11 +15,13 @@ public class ShowDeath implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player){
             Player p = (Player) sender;
-            p.getStatistic(Statistic.DEATHS);
-
-
-
-
+            int statistic = p.getStatistic(Statistic.DEATHS);
+            Bukkit.getScheduler().runTaskTimer(StayAlive.instance, new BukkitRunnable() {
+                @Override
+                public void run() {
+                    p.sendActionBar("your deaths - " + statistic);
+                }
+            }, 5 , 5);
 
         }
         return true;
